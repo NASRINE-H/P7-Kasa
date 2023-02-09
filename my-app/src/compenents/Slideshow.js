be5f461961d6210899d2689904b79d3ed9d9a3f0
NASRINE-H <64153322+NASRINE-H@users.noreply.github.com>
2023-02-09 21:00:44 +0100

    fiche logement

--- my-app/src/compenents/Slideshow.js
+++ my-app/src/compenents/Slideshow.js
@@ -9,17 +9,22 @@ import ImageRight from '../compenents/Images/VectorRight.svg'
     const pictures = foundProduct.pictures;
   
     const [current, setCurrent] = useState(0);
+    //Fonction qui permet d'afficher l'élement suivant du slider
     const nextSlide = () => {
         setCurrent(current === pictures.length - 1 ? 0 : current + 1);
       };
+      //Fonction qui permet d'afficher l'élement précédent du slider
       const prevSlide = () => {
         setCurrent(current === 0 ? pictures.length - 1 : current - 1);
+        // if (current === 0) {
+        //     setCurrent(pictures.length - 1)
+        // } else {
+        //     setCurrent(current - 1)
+        // };
       };
       return (
         <div className="slider">
           {/* Showing slider navigation buttons */}
-          
-          
           <button className="vectorLeft">
             <img src={ImageLeft} onClick={prevSlide} alt="fleche" />
           </button>
